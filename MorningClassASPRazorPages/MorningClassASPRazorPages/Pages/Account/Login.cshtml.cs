ade8deed1e68a5d3c72555066bb82eea0e70b053
Louise <wddtechnologies@gmail.com>
2023-12-20 11:58:08 -0500

    ASP.NETRasorPages
    Student example and recourses on using ASP,NET RASOR PAGES Authentication and Authorization

---
+++ MorningClassASPRazorPages/MorningClassASPRazorPages/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace MorningClassASPRazorPages.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; } = new Credential();
+        public void OnGet()
+        {
+        }
+    }
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
